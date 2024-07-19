534992c453ef4582cd899de059199e900a566898
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-19 10:34:33 +0000

    Deploying to gh-pages from @ anitabaron/goit-js-hw-12@8c60379a19e7ff22f4d5afc7dbe155cab1354caa 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,27 +1,27 @@
-import{i as c,S as h}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const g="44946151-dc02d84f49eea13b7d5c48659",y="https://pixabay.com/api/",b=document.querySelector(".search-form"),u=document.querySelector(".input-field"),m=document.querySelector(".gallery-result-list"),q=document.querySelector(".query-word"),d=t=>({enable:()=>document.querySelector(t).classList.remove("disabled"),disable:()=>document.querySelector(t).classList.add("disabled")}),n=d(".spinner"),a=d(".loading-text"),p=d(".query-text");b.addEventListener("submit",t=>{t.preventDefault();const s=u.value;v(s)});function v(t){const s=`${y}?key=${g}&q=${encodeURIComponent(t)}&image_type=photo&orientation=horizontal&safesearch=true`;n.enable(),a.enable(),fetch(s).then(o=>{if(!o.ok)throw new Error(`Error! status: ${o.status}`);return o.json()}).then(o=>{n.disable(),a.disable(),p.enable(),q.textContent=u.value,o.hits?S(o.hits):c.warning({message:"Sorry, there are no images matching your search query. Please try again!",backgroundColor:"#ef4040",messageColor:"#fff"})}).catch(o=>{n.disable(),a.disable(),console.error(o),c.warning({title:"Error",message:"An error occurred while fetching images"})})}const L="Sorry, there are no images matching your search query. Please try again!";function S(t){if(m.innerHTML="",t.length===0){n.disable(),a.disable(),p.disable(),c.warning({message:L,backgroundColor:"#ef4040",messageColor:"#fff",position:"topRight",timeout:2e3}),setTimeout(f,2e3);return}const s=t.map(w).join("");m.insertAdjacentHTML("beforeend",s),setTimeout(f,500),$.refresh()}function f(){u.value=""}const $=new h(".gallery-result-list a",{captions:!0,captionsData:"alt",captionDelay:250,close:!0,className:"simpleLightboxGallery",doubleTapZoom:2,scrollZoom:!0,overlay:!0});function w({webformatURL:t,tags:s,downloads:o,likes:i,comments:e,views:r}){return`<li class="list-container">
-    <div >
+import{a as g,i as u,S as y}from"./assets/vendor-c493984e.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const h="44946151-dc02d84f49eea13b7d5c48659",b="https://pixabay.com/api/",q=document.querySelector(".search-form"),d=document.querySelector(".input-field"),m=document.querySelector(".gallery-result-list"),v=document.querySelector(".query-word"),c=r=>({enable:()=>document.querySelector(r).classList.remove("disabled"),disable:()=>document.querySelector(r).classList.add("disabled")}),n=c(".spinner"),l=c(".loading-text"),p=c(".query-text"),L=c(".more");q.addEventListener("submit",r=>{r.preventDefault();const o=d.value;S(o)});async function S(r,o=1){n.enable(),l.enable();try{const s=await g.get(b,{params:{key:h,q:r,image_type:"photo",orientation:"horizontal",safesearch:"true",page:o,per_page:30}});return console.log("Posts: ",s.data),s.data.hits?(x(s.data.hits),n.disable(),l.disable(),p.enable(),L.enable(),v.textContent=d.value):u.warning({message:"Sorry, there are no images matching your search query. Please try again!",backgroundColor:"#ef4040",messageColor:"#fff"}),s.data}catch(s){n.disable(),l.disable(),console.error(s),u.warning({title:"Error",message:"An error occurred while fetching images"})}}const w="Sorry, there are no images matching your search query. Please try again!";function x(r){if(m.innerHTML="",r.length===0){n.disable(),l.disable(),p.disable(),u.warning({message:w,backgroundColor:"#ef4040",messageColor:"#fff",position:"topRight",timeout:2e3}),setTimeout(f,2e3);return}const o=r.map(P).join("");m.insertAdjacentHTML("beforeend",o),setTimeout(f,500),T.refresh()}function f(){d.value=""}const T=new y(".gallery-result-list a",{captions:!0,captionsData:"alt",captionDelay:250,close:!0,className:"simpleLightboxGallery",doubleTapZoom:2,scrollZoom:!0,overlay:!0});function P({webformatURL:r,largeImageURL:o,tags:s,downloads:i,likes:e,comments:t,views:a}){return`<li class="list-container">
+    <div>
       <div class="image-container">
-        <a href="${t}" data-source="${t}">
-          <img src="${t}" alt="${s}" />
+        <a href="${o}">
+          <img src="${r}" alt="${s}" />
         </a>
       </div>
       <div class="descr-element">
         <ul class="descr-list">
           <li>
             <h3>Likes</h3>
-              <p>${i}</p>
+              <p>${e}</p>
           </li>
           <li>
             <h3>Views</h3>
-              <p>${r}</p>
+              <p>${a}</p>
           </li>
           <li>
             <h3>Comments</h3>
-            <p>${e}</p>
+            <p>${t}</p>
           </li>
           <li>
             <h3>Downloads</h3>
-              <p>${o}</p>
+              <p>${i}</p>
           </li>
         </ul>
       </div>
